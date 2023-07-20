730b7045262b5617a4c590c074506162b7609bec
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2023-07-20 21:56:58 +0530

    add: tailwind css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-pink-500 text-center font-extrabold">Weekly</h1>
+      <p className="text-pink-300 text-center font-bold">
+        No Dates, Just Weekdays!
+      </p>
     </div>
   );
 }
