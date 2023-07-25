968ec8b9b4fd35ba1b073d744fbe29c718207742
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2023-07-25 22:58:16 +0530

    add: signout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,36 @@
 import React from "react";
 import { auth } from "../firebase";
 
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 function Dashboard() {
-  return <div>Dashboard of {auth.currentUser.email}</div>;
+  const navigate = useNavigate();
+  const logout = () => {
+    try {
+      signOut(auth)
+        .then(() => {
+          // Sign-out successful.
+          console.log("Log out Success");
+          navigate("/login");
+        })
+        .catch((error) => {
+          // An error happened.
+          console.log("error", error);
+        });
+    } catch (error) {
+      alert("Error occured", error);
+    }
+  };
+
+  return (
+    <div>
+      <h3>Dashboard of {auth.currentUser.email}</h3>
+      <button className="bg-red-500 text-white" onClick={logout}>
+        Logout
+      </button>
+    </div>
+  );
 }
 
 export default Dashboard;
