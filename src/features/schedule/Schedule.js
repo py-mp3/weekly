5890db720cab1798903695b4909c9ca169a6a31d
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2023-07-28 11:51:23 +0530

    feat: schedule sync with firebase

--- src/features/schedule/Schedule.js
+++ src/features/schedule/Schedule.js
@@ -1,22 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import data from "./data.json";
 import moment from "moment-timezone";
 
 import { db, auth } from "../../firebase";
-import { doc, setDoc } from "firebase/firestore";
-import { updateEmail } from "firebase/auth";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
 function Schedule() {
   const [updated, setupdated] = useState(0);
   const [schedule, setSchedule] = useState(data.schedule);
+  const [savedChanges, setSavedChanges] = useState(true);
   const days = Object.keys(data.schedule);
   const timings = data.schedule[days[0]].map((slot) => slot.timeUTC);
 
   const saveChanges = async () => {
     // Add a new document in collection "cities"
-    await setDoc(doc(db, "users/" + auth.currentUser.email), {
-      schedule: schedule,
-    });
+    try {
+      await setDoc(doc(db, "users/" + auth.currentUser.email), {
+        schedule: schedule,
+      });
+      setSavedChanges(true);
+    } catch {
+      alert("Unable to connect!");
+    }
   };
 
   const editSchedule = (day, time, update) => {
@@ -24,15 +30,40 @@ function Schedule() {
     updatedSchedule[day].find((slot) => slot.timeUTC === time).label = update;
     setSchedule(updatedSchedule);
     setupdated((updated) => updated + 1);
+    setSavedChanges(false);
   };
+
+  const getLatestSchedule = async () => {
+    const docRef = doc(db, "users/" + auth.currentUser.email);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      setSchedule(docSnap.data().schedule);
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  };
+
+  useEffect(() => {
+    getLatestSchedule();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        getLatestSchedule();
+      }
+    });
+  }, []);
 
   return (
     <div className="p-4">
       <h1>Main time {moment().format("MMMM Do YYYY, h:mm:ss a")}</h1>
       <h1>JS : {new Date().toString()}</h1>
       <h2 className="text-2xl font-bold mb-4">Weekly Schedule</h2>
-      <button onClick={saveChanges}>Save changes</button>
+      <button className="bg-yellow-300 p-2 rounded-md" onClick={saveChanges}>
+        Save changes
+      </button>
       <h3>Update : {updated}</h3>
+      <h3>Saved changes : {savedChanges.toString()}</h3>
       <table className="table-auto w-full">
         <thead>
           <tr>
@@ -52,7 +83,7 @@ function Schedule() {
                 <td
                   key={`${day}-${time}`}
                   className={`border px-4 py-2 ${
-                    data.schedule[day].find((slot) => slot.timeUTC === time)
+                    schedule[day].find((slot) => slot.timeUTC === time)
                       .label === "free"
                       ? "bg-green-300"
                       : "bg-red-300"
@@ -61,7 +92,7 @@ function Schedule() {
                   <div className="d-flex flex justify-between">
                     <span>
                       {
-                        data.schedule[day].find((slot) => slot.timeUTC === time)
+                        schedule[day].find((slot) => slot.timeUTC === time)
                           .label
                       }
                     </span>
